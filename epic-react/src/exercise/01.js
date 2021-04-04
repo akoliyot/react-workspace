f641f88b8181a9a5380e27816dc82854dcf97515
Aswin Koliyot <akoliyot@gmail.com>
2021-04-04 10:56:44 +0200

    Answer to 01.js EC4

--- epic-react/src/exercise/01.js
+++ epic-react/src/exercise/01.js
@@ -3,18 +3,21 @@
 
 import * as React from "react";
 
-const countReducer = (prevState, action) => ({
-  ...prevState,
-  ...(typeof action === "function" ? action(prevState) : action),
-});
+const countReducer = (prevState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: prevState.count + action.step };
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`);
+  }
+};
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   });
   const { count } = state;
-  const increment = () =>
-    setState((currentState) => ({ count: currentState.count + step }));
+  const increment = () => dispatch({ type: "INCREMENT", step });
 
   return <button onClick={increment}>{count}</button>;
 }

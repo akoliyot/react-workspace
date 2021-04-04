1afb4fa29d6559cc9c6251f48ddec45d26061d1b
Aswin Koliyot <akoliyot@gmail.com>
2021-04-04 11:20:58 +0200

    Answer to 02.js EC1

--- epic-react/src/exercise/02.js
+++ epic-react/src/exercise/02.js
@@ -28,7 +28,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState, dependencies) {
+function useAsync(asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: "idle",
     data: null,
@@ -52,26 +52,22 @@ function useAsync(asyncCallback, initialState, dependencies) {
         dispatch({ type: "rejected", error });
       }
     );
-    // 🐨 you'll accept dependencies as an array and pass that here.
-    // 🐨 because of limitations with ESLint, you'll need to ignore
-    // the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies);
+  }, [asyncCallback]);
 
   return state;
 }
 
 function PokemonInfo({ pokemonName }) {
-  const state = useAsync(
-    () => {
-      if (!pokemonName) {
-        return;
-      }
-      return fetchPokemon(pokemonName);
-    },
-    { status: pokemonName ? "pending" : "idle" },
-    [pokemonName]
-  );
+  const asyncCallback = React.useCallback(() => {
+    if (!pokemonName) {
+      return;
+    }
+    return fetchPokemon(pokemonName);
+  }, [pokemonName]);
+
+  const state = useAsync(asyncCallback, {
+    status: pokemonName ? "pending" : "idle",
+  });
 
   const { data: pokemon, status, error } = state;
 

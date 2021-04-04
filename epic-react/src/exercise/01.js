9aec7a96a4f2d92f449d68eab8f590c01efe669e
Aswin Koliyot <akoliyot@gmail.com>
2021-04-04 10:31:54 +0200

    Answer to 01.js EC1

--- epic-react/src/exercise/01.js
+++ epic-react/src/exercise/01.js
@@ -3,20 +3,13 @@
 
 import * as React from "react";
 
-function countReducer(prevCount, newCount) {
-  return newCount;
+function countReducer(prevCount, step) {
+  return prevCount + step;
 }
 
 function Counter({ initialCount = 0, step = 1 }) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount);
-
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step);
+  const [count, changeCount] = React.useReducer(countReducer, initialCount);
+  const increment = () => changeCount(step);
   return <button onClick={increment}>{count}</button>;
 }
 
